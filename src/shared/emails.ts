1013dc6c3c3fe30ad07ba2003c56951ffdb05bc5
Ennaim Yassine <ennaimyassine@gmail.com>
2021-06-01 10:34:12 +0100

    change emails content

--- src/shared/emails.ts
+++ src/shared/emails.ts
@@ -1,6 +1,7 @@
 export const CREATE_USER_TEMPLATE = 'create.user';
-export const CREATE_USER_SUBJECT = 'M2M plateforme : Création de compte';
+export const CREATE_USER_SUBJECT = 'M2M plateforme : Compte créé avec succès';
 export const ACTIVE_USER_TEMPLATE = 'active.user';
-export const ACTIVE_USER_SUBJECT = 'Your account has been activated';
+export const ACTIVE_USER_SUBJECT = 'M2M plateforme : Compte activé avec succès';
 export const RESET_PASSWORD_TEMPLATE = 'reset.password';
-export const RESET_PASSWORD_SUBJECT = 'Please reset your password';
+export const RESET_PASSWORD_SUBJECT =
+  'M2M plateforme : Demande de changement de mot de passe';
